99ed7db2d4f657647ef0772329d2c60de3bda582
Vianney418 <123280157+Vianney418@users.noreply.github.com>
2024-12-10 17:23:18 -0300

    Update 2 run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,1 +1,3 @@
-"C:\Users\viann\AppData\Roaming\npm\newman" run "C:\Users\viann\Downloads\test\New_API_PetStore.postman_collection.json" -r cli,html --reporter-html-export "C:\Users\viann\Downloads\test\resultados.html"
+mkdir "C:\ProgramData\Jenkins\.jenkins\workspace\API_Test_PetStore\test"
+
+newman run "C:\Users\viann\Downloads\test\New_API_PetStore.postman_collection.json" -r cli,html --reporter-html-export "C:\ProgramData\Jenkins\.jenkins\workspace\API_Test_PetStore\test\resultados.html"
